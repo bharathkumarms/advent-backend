e2955711fad0d55b572a5e1bf3d75b9a0c8d4234
Bharath V <bharathkumar.v@nielsen.com>
2017-07-13 16:05:33 +0530

    Initial Commit

---
+++ js.advent/js.advent.model/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace js.advent.model
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+    }
+}
---
+++ js.advent/js.advent.webapi/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using js.advent.model;
+
+namespace js.advent.webapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        //// GET api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/login/5
+        [HttpGet("{id}")]
+        public bool Get(int id)
+        {
+            //var validUsers = new List<User>();
+            //var user1 = new User();
+            //user1.UserName = "bharath";
+            //user1.Password = "test123";
+            //validUsers.Add(user1);
+
+            return true;
+            
+        }
+
+        // POST api/values
+        [HttpPost]
+        public User Post([FromBody]string value)
+        {
+            var user1 = new User();
+            user1.firstName = "bharath";
+            user1.lastName = "test123";
+            user1.userName = "test";
+            user1.id = 1;
+            return user1;
+        }
+
+        /*// PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }*/
+    }
+}
